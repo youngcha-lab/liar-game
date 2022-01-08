52187889b510fe10f9b4b14e41e70e45d38bacde
kyle-log <cocomo1316@gmail.com>
2022-01-09 02:11:27 +0900

    First commit

---
+++ liar-game-app/src/test/kotlin/com/youngcha/liargameapp/LiarGameAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.youngcha.liargameapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiarGameAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
