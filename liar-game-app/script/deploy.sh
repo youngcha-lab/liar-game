78ff2311ecde20cedb1bc938b46d91e351da1f40
kjlkggg <kjlkg@naver.com>
2022-01-22 11:42:11 +0900

    Update deploy.sh
    Update deploy shell script

--- liar-game-app/script/deploy.sh
+++ liar-game-app/script/deploy.sh
@@ -1,1 +1,27 @@
-git pull
+# move repository home
+cd /home/ec2-user/apps/liar-game
+
+# update git pull list
+git fetch --all
+
+# reset git to update branch
+git reset --hard origin/main
+
+# start build project
+./liar-game-app/gradlew build
+
+# find pid liar application
+APP_PID=`pgrep liar.jar`
+
+if [ -z $APP_PID]
+then
+        echo "Not process Running"
+else
+        echo "kill current process"
+        kill -9 $APP_PID
+        sleep 5
+fi
+
+echo "new liar application process start"
+
+nohup java -jar liar.jar &
