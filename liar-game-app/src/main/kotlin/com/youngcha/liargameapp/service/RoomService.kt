54da2f2d05121479b194edb40a4a066cce49032c
kyle-log <cocomo1316@gmail.com>
2022-02-17 00:37:39 +0900

    Make Room API

--- liar-game-app/src/main/kotlin/com/youngcha/liargameapp/service/RoomService.kt
+++ liar-game-app/src/main/kotlin/com/youngcha/liargameapp/service/RoomService.kt
@@ -1,16 +1,23 @@
 package com.youngcha.liargameapp.service
 
 import com.youngcha.liargameapp.domain.Room
+import com.youngcha.liargameapp.repository.RoomRepository
 import com.youngcha.liargameapp.utils.UuidGenerator
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
-class RoomService {
+class RoomService(
+    val roomRepository: RoomRepository
+) {
 
-    fun makeRoom(): Room = Room(
-        roomId = 1,
-        roomCode = UuidGenerator.generate(),
-        createdAt = LocalDateTime.now()
-    )
+    fun makeRoom(): Room {
+        val newRoom = Room(
+            roomId = 0,
+            roomCode = UuidGenerator.generate(),
+            createdAt = LocalDateTime.now()
+        )
+        roomRepository.save(newRoom)
+        return newRoom
+    }
 }
