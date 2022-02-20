9dcdca151761a1fd582b8bfc3ac7319b11566c5d
kjlkggg <kjlkg@naver.com>
2022-02-20 18:49:21 +0900

    Update deploy.sh

--- liar-game-app/script/deploy.sh
+++ liar-game-app/script/deploy.sh
@@ -1,1 +1,18 @@
-mkdir /home/ec2-user/apps/liar-game/liar-game-app/script/mytest
+# find pid liar application
+APP_PID=`ps -ef | grep liar-game-app-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
+
+# if application process is already runned, kill the process
+if [ -z $APP_PID ]
+then
+        echo "Not process Running"
+else
+        echo "kill current process"
+        kill -9 $APP_PID
+        sleep 5
+fi
+
+echo "new liar application process start"
+
+# application process start
+cd /home/ec2-user/apps/liar-game/liar-game-app/build/libs
+nohup java -jar liar-game-app-0.0.1-SNAPSHOT.jar &
