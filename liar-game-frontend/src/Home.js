4b03fc0403adc848116ee3e20489f0f05430d4df
Rapha0814 <hans7368@gmail.com>
2022-02-26 22:06:32 +0900

    Home 화면 구현

--- liar-game-frontend/src/Home.js
+++ liar-game-frontend/src/Home.js
@@ -1,38 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "./css/Home.css";
+import { useState } from "react";
+
+function isValidName(name) {
+  if (name === "") {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 function Home() {
+  const [name, setName] = useState("");
+  const onChange = (event) => setName(event.target.value);
+  const creatRoom = (event) => {
+    console.log(isValidName(name));
+  };
   return (
-    <div
-      style={{
-        backgroundColor: "rgba(196, 196, 196, 1)",
-        width: "100%",
-        height: "100%",
-        textAlign: "center",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "382px",
-        }}
-      >
-        <h1 style={{ marginBottom: "200px", fontSize: "72px" }}>LIER GAME</h1>
-        <input
-          style={{
-            marginBottom: "30px",
-            width: "100%",
-            fontSize: "24px",
-            padding: "16px",
-          }}
-        />
-        <br />
-        <button style={{ width: "100%", fontSize: "24px", padding: "16px" }}>
-          방생성
-        </button>
+    <div className="Home">
+      <div className="Home-body">
+        <div className="Home-content">
+          <h1>LIER GAME</h1>
+          <input onChange={onChange} value={name} />
+          <br />
+          <button onClick={creatRoom}>방생성</button>
+        </div>
       </div>
     </div>
   );
