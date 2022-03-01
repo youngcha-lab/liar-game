4fc1b30c67c3106dbe731d4e4ecc53f058f949bd
kyle-log <cocomo1316@gmail.com>
2022-03-01 22:31:53 +0900

    Rename controllers

--- liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/PingRestController.kt
+++ liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/PingRestController.kt
@@ -1,13 +1,13 @@
 package com.youngcha.liargameapp.controller
 
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GameController {
+class PingRestController {
 
-    @PostMapping("/api/game/start")
-    fun start(): String {
+    @GetMapping("/api/ping")
+    fun ping(): String {
         return "pong"
     }
 }
--- liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/RoomRestController.kt
+++ liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/RoomRestController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RoomController(
+class RoomRestController(
     val roomService: RoomService
 ) {
 
