ca4b417edd166ec607c98afee338dc9dd5a752b8
Rapha0814 <hans7368@gmail.com>
2022-03-03 00:41:11 +0900

    Front 구조 수정

--- liar-game-frontend/src/index.js
+++ liar-game-frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./css/index.css";
 import App from "./App";
-import Home from "./Home";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
