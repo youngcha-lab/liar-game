26aef57761108f965abf3ce2044771d176062e7c
kjlkggg <kjlkg@naver.com>
2022-03-07 00:11:47 +0900

    Update deploy.sh

--- liar-game-frontend/script/deploy.sh
+++ liar-game-frontend/script/deploy.sh
@@ -4,11 +4,11 @@ cd /home/ec2-user/apps/liar-game/liar-game-frontend
 
 echo " frontend deploy 시작" >> front_debug.txt 
 # module install
-nohup npm install &
+nohup npm install & >> front_debug.txt
 echo " npm install 완료" >> front_debug.txt
 # build
-nohup npm run build &
+nohup npm run build & >> front_debug.txt
 echo " npm build 완료" >> front_debug.txt
 
 
-nohup /root/.nvm/versions/node/v17.4.0/bin/npx serve -s -n build &
+nohup /root/.nvm/versions/node/v17.4.0/bin/npx serve -s -n build & >> front_debug.txt
