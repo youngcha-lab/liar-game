61e09cd90783f6d72bda71a627d41fc0ec557ab3
kjlkggg <kjlkg@naver.com>
2022-03-06 21:04:02 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,4 @@
+./home/ec2-user/apps/liar-game/liar-game-app/script/before-install.sh
 ./home/ec2-user/apps/liar-game/liar-game-app/script/deploy.sh
 ./home/ec2-user/apps/liar-game/liar-game-frontend/script/deploy.sh
 
