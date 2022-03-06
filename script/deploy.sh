2b3ef57f007b9a3fd1c51dd4a59118a1b13fd2dc
kjlkggg <kjlkg@naver.com>
2022-03-06 22:34:11 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,15 +1,15 @@
 mkdir /home/ec2-user/apps/liar-game/liar-game-app/script/debug
 cd /home/ec2-user/apps/liar-game/liar-game-app/script
 chmod 755 before-install.sh
-./before-install.sh >> debug.txt
+./before-install.sh 
 echo "before 실행완료" >> debug.txt
 sleep 10
 cd /home/ec2-user/apps/liar-game/liar-game-app/script
 chmod 755 deploy.sh
 ./deploy.sh >> debug.txt
-echo "app deploy 실행완료" >> debug.txt
+echo "app deploy 실행완료" 
 sleep 10
 cd /home/ec2-user/apps/liar-game/liar-game-frontend/script
 chmod 755 deploy.sh
 ./deploy.sh >> debug.txt
-echo "front deploy 실행완료" >> debug.txt
+echo "front deploy 실행완료" 
