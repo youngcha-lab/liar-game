ca09cfb7046ab3918246e79a55c9faa930e1146e
kjlkggg <kjlkg@naver.com>
2022-03-07 00:03:57 +0900

    Update deploy.sh

--- liar-game-frontend/script/deploy.sh
+++ liar-game-frontend/script/deploy.sh
@@ -9,23 +9,6 @@ echo " npm install 완료" >> front_debug.txt
 # build
 npm run build 
 echo " npm build 완료" >> front_debug.txt
-# find pid application
-NODE_PID=`ps -ef | grep '/bin/serve -s -n build' | grep -v grep | awk '{print $2}'`
 
-# if application process is already runned, kill the process
-if [ -z $NODE_PID ]
-then
-        echo "Not process Running" >> front_debug.txt
-else
-        echo "kill current process" >> front_debug.txt
-        kill -9 $NODE_PID
-        sleep 5
-fi
 
-echo "new application process start" >> front_debug.txt
-
-# application process start
-nohup npx serve -s -n build & >> front_debug.txt
-NODE_PID=`ps -ef | grep '/bin/serve -s -n build' | grep -v grep | awk '{print $2}'`
-echo "NodePID: $NODE_PID" >> front_debug.txt
-echo "new application process start 실행완료" >> front_debug.txt 
+nohup /root/.nvm/versions/node/v17.4.0/bin/npx serve -s -n build &
