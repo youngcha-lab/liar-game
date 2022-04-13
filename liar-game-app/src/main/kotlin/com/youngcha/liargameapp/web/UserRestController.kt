a751d106ae3d38011b9583f3e86a40108543ee2e
kyle-log <cocomo1316@gmail.com>
2022-04-13 20:32:12 +0900

    Request body nickname

--- liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/UserRestController.kt
+++ liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/UserRestController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.Cookie
@@ -24,7 +25,7 @@ class UserRestController(
     @PostMapping
     fun createUser(
         @PathVariable("room_code") roomCode: String,
-        nickname: String,
+        @RequestBody nickname: String,
         httpServletResponse: HttpServletResponse
     ): ResponseEntity<*> {
         val userCode = userCreateProcessor.createUser(
