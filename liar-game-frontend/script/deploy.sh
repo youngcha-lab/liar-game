949ded42f12803896b919f051822f197ae1943b1
kjlkggg <kjlkg@naver.com>
2022-05-01 19:43:54 +0900

    Create deploy.sh

---
+++ liar-game-frontend/script/deploy.sh
@@ -0,0 +1,11 @@
+cd /home/ec2-user/apps/liar-game/liar-game-frontend
+
+# module install
+/usr/bin/nohup /root/.nvm/versions/node/v17.9.0/bin/npm install 
+sleep 10
+
+# build
+/usr/bin/nohup /root/.nvm/versions/node/v17.9.0/bin/npm run build
+sleep 10
+
+/usr/bin/nohup /root/.nvm/versions/node/v17.9.0/bin/npm start &
