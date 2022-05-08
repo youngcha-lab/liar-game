a024b02f04a3a70a293c4c87f607e5133f9fdb2d
kjlkggg <kjlkg@naver.com>
2022-05-08 16:54:10 +0900

    Update before-install.sh

--- liar-game-app/script/before-install.sh
+++ liar-game-app/script/before-install.sh
@@ -4,5 +4,3 @@ if [ -d /home/ec2-user/apps/liar-game ]; then
     rm -rf /home/ec2-user/apps/liar-game
 fi
 mkdir -vp /home/ec2-user/apps/liar-game
-cp -pr /home/ec2-user/apps/liar-game/liar-game-app/build/libs/liar-game-app-0.0.1-SNAPSHOT.jar /home/ec2-user/apps/liar-game/liar-game-app/script/
-cp -pr /work/docker/backend/OpenJDK11U-jdk_x64_linux_hotspot_11.0.14.1_1.tar.gz /home/ec2-user/apps/liar-game/liar-game-app/script/
