d56259127655ab48f7bb5fbee8f8af01b4a3538e
kjlkggg <kjlkg@naver.com>
2022-05-08 19:43:12 +0900

    Update deploy.sh

--- liar-game-frontend/script/deploy.sh
+++ liar-game-frontend/script/deploy.sh
@@ -3,8 +3,8 @@ cd /home/ec2-user/apps/liar-game/liar-game-frontend
 export PATH=$PATH
 
 # module install
-/usr/bin/nohup >> /work/0508.txt
-#/root/.nvm/versions/node/v17.9.0/bin/npm install 
+#/usr/bin/nohup 
+/root/.nvm/versions/node/v17.9.0/bin/npm install >> /work/0508.txt
 sleep 10
 
 # build
