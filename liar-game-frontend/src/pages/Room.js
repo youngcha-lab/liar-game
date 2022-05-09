69c564b29764cf596c087ed7d104f22292184556
김승욱 <gsces6000@gmail.com>
2022-05-09 16:27:15 +0900

    sidebar and before game start develop

--- liar-game-frontend/src/pages/Room.js
+++ liar-game-frontend/src/pages/Room.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import Avatar from "@mui/material/Avatar";
-import { Card, CardHeader } from "@mui/material";
 import "../css/Room.css";
 import axios from "axios";
+import "../img/crown.png";
 
 function Room() {
   const [word, setWord] = useState("");
   const [category, setCategory] = useState("");
+  const [leader, setLeader] = useState("");
   const [users, setUsers] = useState("");
   const [userCnt, setUserCnt] = useState(1);
-
+  
   const location = useLocation();
   const navigate = useNavigate();
-  
+ 
   const host = "http://" + window.location.hostname;
   const url = location.pathname.split("/");
   const roomCode = url[url.length - 1];
@@ -21,9 +21,11 @@ function Room() {
   const checkUser = async () => {
     const response = await axios.get(host + `:8080/api/v1/room/${roomCode}`);
     console.log(response);
-
+    
+    setLeader(response.data.room.leader);
     setUsers(response.data.room.users);
-    setUserCnt(response.data.room.users.length);    
+    setUserCnt(response.data.room.users.length);
+           
     const currentUser = response.data.room.currentUser;
     if (!currentUser || !currentUser.isMember) {
        console.log("current user is not valid");
@@ -31,20 +33,19 @@ function Room() {
     }
   };
 
-  // useEffect(() => {
-  //   checkUser();    
-  // }, [location]);
-
   useEffect(() => {
+    checkUser();
+    
     const loop = setInterval(()=> {
       checkUser();      
-    }, 10000);         
+    }, 1000);         
   }, []);
 
-  // const randomColor = () => {
-  //   let color = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-  //   return color;
-  // };
+  const randomColor = () => {
+    let color = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+    console.log("@@randomColor: " + color);
+    return color;
+  };
 
   const onLinkClick = () => {
     const copyText = host + ":3000" + location.pathname;
@@ -60,7 +61,6 @@ function Room() {
       const response = await axios.post(
         host + `:8080/api/v1/room/${roomCode}/game/start`
       );
-      console.log(response);
       setWord(response.data.keyword);
       setCategory(response.data.category);
       return response;
@@ -85,26 +85,17 @@ function Room() {
         </div>
         <div className="playerNumber">플레이어 {userCnt} / 10</div>
         <div className="playerContainer">
-          <div className="playerItem">
-            <div className="playerThumbnail"></div>
-            김승욱
+          {users && users.map((user) => (
+          <div className="playerItem" key={user}>            
+            {
+              (leader === user)  
+              ? <div className="leaderThumbnail"></div> 
+              : <div className="playerThumbnail"></div>   
+            }            
+            {user}
           </div>
+          ))} 
         </div>
-        {/* <Card sx={{ maxWidth: 345, bgcolor: "#C4C4C4", color: "black" }}>
-            <CardHeader
-              avatar={<Avatar aria-label="recipe"></Avatar>}
-              title="김승욱"
-            />
-        </Card> */}
-        {/* {users.map((user) => (
-          <Card sx={{ maxWidth: 345, bgcolor: "#C4C4C4", color: "black" }}>
-            <CardHeader
-              avatar={<Avatar aria-label="recipe"></Avatar>}
-              key={user}
-              title={user}
-            />
-          </Card>
-        ))}   */}
         <div className="exit_button">
           <Link to={"/Home"}>
             나가기
