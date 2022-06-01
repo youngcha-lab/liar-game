3dd285072d7fc9dfbf25d2405906d8592ab02e5c
kjlkggg <kjlkg@naver.com>
2022-06-01 21:33:36 +0900

    Update deploy.sh

--- liar-game-frontend/script/deploy.sh
+++ liar-game-frontend/script/deploy.sh
@@ -1,17 +1,25 @@
-cd /home/ec2-user/apps/liar-game/liar-game-frontend
+# find pid liar react application 
+APP_PID=`docker ps -a | grep react | grep Up`
 
-sleep 300
+# if application process is already runned, kill the process
+if [ -z $APP_PID ]
+then
+        echo "Not process Running" 
+else
+        echo "kill current process" 
+        docker stop react
+        sleep 5
+        docker rm react
+        sleep 5
+fi
 
-# module install
-nohup npm install 
+echo "new liar react application process start"
 
-sleep 10
+# docker application container build
+docker image rm frontendimage
+docker build -t frontendimage .
 
-mkdir /work/test0601
+sleep 5
 
-# build
-nohup npm run build
-
-sleep 10
-
-nohup npm start
+# docker react application container start 
+docker run -p 3000:3000 -v /home/ec2-user/apps/liar-game/liar-game-frontend:/app -d --name react frontendimage
