20dc51135a76646ecac89e63885ea56d98f002d8
kyle-log <cocomo1316@gmail.com>
2022-07-05 19:14:33 +0900

    Allow content type header

--- liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/WebConfig.kt
+++ liar-game-app/src/main/kotlin/com/youngcha/liargameapp/web/WebConfig.kt
@@ -2,6 +2,7 @@ package com.youngcha.liargameapp.web
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -12,11 +13,12 @@ class WebConfig(val environment: Environment) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         println("environment : ${environment.activeProfiles.toList()}")
         registry.addMapping("/**")
-            .allowedOrigins(
+            .allowedHeaders(
+                HttpHeaders.CONTENT_TYPE.lowercase()
+            ).allowedOrigins(
                 "http://localhost:3000",
                 "http://youngcha-liargame.tk:3000",
-            )
-            .allowedMethods(
+            ).allowedMethods(
                 HttpMethod.GET.name,
                 HttpMethod.HEAD.name,
                 HttpMethod.POST.name,
